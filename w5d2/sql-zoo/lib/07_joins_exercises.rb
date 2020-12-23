f6df0cd92f3092de52dc6c2517deeea952839cbc
your name here <rupesh_singh87@yahoo.com>
2020-12-23 17:11:41 -0600

    practising for exam

--- w5d2/sql-zoo/lib/07_joins_exercises.rb
+++ w5d2/sql-zoo/lib/07_joins_exercises.rb
@@ -1,5 +1,23 @@
 # == Schema Information
 #
+
+
+require_relative './sqlzoo.rb'
+
+def example_join
+  execute(<<-SQL)
+    SELECT
+      *
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      actors.name = 'Sean Connery'
+  SQL
+end
 # Table name: actors
 #
 #  id          :integer      not null, primary key
@@ -20,26 +38,20 @@
 #  actor_id    :integer      not null, primary key
 #  ord         :integer
 
-require_relative './sqlzoo.rb'
-
-def example_join
-  execute(<<-SQL)
-    SELECT
-      *
-    FROM
-      movies
-    JOIN
-      castings ON movies.id = castings.movie_id
-    JOIN
-      actors ON castings.actor_id = actors.id
-    WHERE
-      actors.name = 'Sean Connery'
-  SQL
-end
-
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+    select 
+      title
+    from 
+      movies
+    join castings 
+      on castings.movie_id = movies.id
+    join actors
+      on castings.actor_id = actors.id
+    where
+      name = 'Harrison Ford'
+
   SQL
 end
 
@@ -48,12 +60,33 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+    select 
+      title
+    from 
+      movies
+    join castings 
+      on castings.movie_id = movies.id
+    join actors
+      on castings.actor_id = actors.id
+    where
+      name = 'Harrison Ford' and ord != 1
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+  select 
+      movies.title , actors.name
+    from 
+      movies
+    join castings 
+      on castings.movie_id = movies.id
+    join actors
+      on castings.actor_id = actors.id
+    where
+      yr = 1962 and ord = 1
+
   SQL
 end
 
@@ -61,6 +94,18 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    select 
+      movies.yr , count(*)
+    from 
+      movies
+    join castings 
+      on castings.movie_id = movies.id
+    join actors
+      on castings.actor_id = actors.id
+    where actors.name = 'John Travolta'
+    group by
+       movies.yr
+    having count( * ) >= 2 
   SQL
 end
 
@@ -68,6 +113,23 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+  select 
+    movies.title , actors.name
+  from movies
+  join castings 
+      on castings.movie_id = movies.id
+  join actors
+      on castings.actor_id = actors.id
+  where movies.id in (
+    select 
+    movies.id
+  from movies
+  join castings 
+      on castings.movie_id = movies.id
+  join actors
+      on castings.actor_id = actors.id
+  where actors.name = 'Julie Andrews'
+  ) and ord = 1
   SQL
 end
 
